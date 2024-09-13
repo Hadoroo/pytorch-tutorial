703a1a5e37be93548ad5eb8222d7a07fcefae3b6
Hadoroo <128714895+Hadoroo@users.noreply.github.com>
2024-09-13 22:35:11 +0700

    change

--- tutorials/01-basics/feedforward_neural_network/main.py
+++ tutorials/01-basics/feedforward_neural_network/main.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 
+import matplotlib.pyplot as plt
+
+
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -38,20 +41,60 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size) 
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)  
+        self.fc1 = nn.Linear(input_size, hidden_size) #inisiasi input layer
+        
+        #inisiasi output layer
+        self.fc2 = nn.Linear(hidden_size, hidden_size)  
+        self.fc3 = nn.Linear(hidden_size, hidden_size)
+        self.fc4 = nn.Linear(hidden_size, hidden_size)
+        self.fc5 = nn.Linear(hidden_size, hidden_size)
+        self.fc6 = nn.Linear(hidden_size, hidden_size)
+        self.fc7 = nn.Linear(hidden_size, hidden_size)
+        self.fc8 = nn.Linear(hidden_size, hidden_size)
+        self.fc9 = nn.Linear(hidden_size, hidden_size)
+        
+        self.fc10 = nn.Linear(hidden_size, num_classes) #inisiasi output layer
+        self.relu = nn.ReLU() #fungsi aktivasi ReLU
+        self.sigmoid = nn.Sigmoid() #fungsi aktifasi sigmoid
     
     def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
+        out = self.fc1(x) #memasukkan data ke dalam input layer
+        out = self.relu(out) #melakukan fungsi aktivasi untuk hasil data di input layer
+        
+         #memasukkan data ke dalam hidden layer dan melakukan fungsi aktivasi
         out = self.fc2(out)
+        out = self.sigmoid(out)
+        
+        out = self.fc3(out)
+        out = self.relu(out)
+        
+        out = self.fc4(out)
+        out = self.sigmoid(out)
+        
+        out = self.fc5(out)
+        out = self.relu(out)
+        
+        out = self.fc6(out)
+        out = self.sigmoid(out)
+        
+        out = self.fc7(out)
+        out = self.relu(out)
+        
+        out = self.fc8(out)
+        out = self.sigmoid(out)
+        
+        out = self.fc9(out)
+        out = self.relu(out)
+        
+        #memasukkan data ke dalam output layer
+        out = self.fc10(out)
         return out
 
-model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+model = NeuralNet(input_size, hidden_size, num_classes).to(device) #pembuatan model
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
+loss_list = []
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
 
 # Train the model
@@ -62,18 +105,16 @@ for epoch in range(num_epochs):
         images = images.reshape(-1, 28*28).to(device)
         labels = labels.to(device)
         
-        # Forward pass
+        # Forward pass (hitung prediksi model)
         outputs = model(images)
         loss = criterion(outputs, labels)
         
-        # Backward and optimize
+        # Backward and optimize (mengubah gradien ke 0 sebelum menjalankan backward dan mengubah weight berdasarkan gradien)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         
-        if (i+1) % 100 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+        loss_list.append(loss.item())
 
 # Test the model
 # In test phase, we don't need to compute gradients (for memory efficiency)
@@ -92,3 +133,12 @@ with torch.no_grad():
 
 # Save the model checkpoint
 torch.save(model.state_dict(), 'model.ckpt')
+
+plt.figure(figsize=(10, 5))
+plt.plot(loss_list, label='default')
+plt.xlabel('Iteration')
+plt.ylabel('Loss')
+plt.title('Loss table')
+plt.legend()
+plt.grid(True)
+plt.show()
